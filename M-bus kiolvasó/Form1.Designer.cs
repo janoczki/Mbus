4cd25d698c1a4647421e417e0995526a1a515686
Jánóczki András <93191103+janoczki@users.noreply.github.com>
2021-10-26 12:43:03 +0200

    Update Form1.Designer.cs

--- M-bus kiolvasó/Form1.Designer.cs
+++ M-bus kiolvasó/Form1.Designer.cs
@@ -46,7 +46,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(142, 29);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Manuális kolvasás";
+            this.button1.Text = "Manuális kolvasás ";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
